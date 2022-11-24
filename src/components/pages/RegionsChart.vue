c47d3a5603d7a48d1cc34053ca590b7fef7a5578
sleeply <sheroff01@mail.ru>
2022-11-24 10:17:50 +0500

    control animation

--- src/components/pages/RegionsChart.vue
+++ src/components/pages/RegionsChart.vue
@@ -72,49 +72,20 @@ const plugins = {
     },
 }
 
-const totalDuration = 10000;
-const delayBetweenPoints = totalDuration / data.length;
-const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
-
-const animation = {
-  x: {
-    type: 'number',
-    easing: 'linear',
-    duration: delayBetweenPoints,
-    from: NaN, // the point is initially skipped
-    delay(ctx) {
-      if (ctx.type !== 'data' || ctx.xStarted) {
-        return 0;
-      }
-      ctx.xStarted = true;
-      return ctx.index * delayBetweenPoints;
-    }
-  },
-  y: {
-    type: 'number',
-    easing: 'linear',
-    duration: delayBetweenPoints,
-    from: previousY,
-    delay(ctx) {
-      if (ctx.type !== 'data' || ctx.yStarted) {
-        return 0;
-      }
-      ctx.yStarted = true;
-      return ctx.index * delayBetweenPoints;
-    }
-  }
-};
-
 const config = {
     type: 'line',
     data,
     options: {
-        animation,
-        interaction: {
-            intersect: false
+        animation: {
+            tension: {
+                duration: 200,
+                from: 0,
+                to: 1,
+                loop: true
+
+            }
         },
         scales: {
-
             x: {
                 border: {
                     display: true
@@ -124,24 +95,11 @@ const config = {
                     drawOnChartArea: false,
                     drawTicks: true,
                 }
-                // border: {
-                //     display: false
-                // },
-                // grid: {
-                //     display: true,
-                //     drawOnChartArea: true,
-                //     drawTicks: true
-                // }
             },
             y: {
                 border: {
                     display: false
                 },
-                // beginAtZero: true,
-                // suggestedMax: 100,
-                // ticks: {
-                //     stepSize: 20
-                // },
 
             },
         },
